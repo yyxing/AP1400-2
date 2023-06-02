dda69141a72934c36c8ce1234fae57f8daf81164
Zhangfubiao <274043619@qq.com>
2023-06-02 17:33:58 +0800

    Update hw1.cpp

--- AP1400-2-HW1/src/hw1.cpp
+++ AP1400-2-HW1/src/hw1.cpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <iomanip>
 static inline void check_empty(std::initializer_list<Matrix> ilm) {
-  for (auto it = ilm.begin(); it != ilm.end(); ++it) {
-    if (it->empty()) {
+  for (const auto &it : ilm) {
+    if (it.empty()) {
       throw std::logic_error("matrix must be non-empty");
     }
   }
@@ -14,6 +14,12 @@ static inline void check_empty(std::initializer_list<Matrix> ilm) {
 static inline void check_size(size_t n, size_t m) {
   if (n == 0 || m == 0) {
     throw std::invalid_argument("size must be non-zero");
+  }
+}
+
+static inline void check_square(const Matrix &mat) {
+  if (mat.size() != mat[0].size()) {
+    throw std::logic_error("matrix must be square");
   }
 }
 
@@ -111,6 +117,112 @@ Matrix sum(const Matrix &matrix1, const Matrix &matrix2) {
   }
   return matrix;
 }
+Matrix transpose(const Matrix &matrix) {
+  size_t n1, m1;
+  n1 = matrix.size();
+  if (n1 == 0) return matrix;
+  m1 = matrix[0].size();
+  Matrix mat = zeros(m1, n1);
+  for (int i = 0; i < n1; ++i) {
+    for (int j = 0; j < m1; ++j) {
+      mat[j][i] = matrix[i][j];
+    }
+  }
+  return mat;
+}
+// 将矩阵转换成
+Matrix minor(const Matrix &matrix, size_t n, size_t m) {
+  check_empty({matrix});
+  size_t n1 = matrix.size(), m1 = matrix[0].size();
+  Matrix mat = zeros(n1 - 1, m1 - 1);
+  for (int i = 0; i < n1 - 1; ++i) {
+    for (int j = 0; j < m1 - 1; ++j) {
+      int ii = i < n ? i : i + 1;
+      int jj = j < m ? j : j + 1;
+      mat[i][j] = matrix[ii][jj];
+    }
+  }
+  return mat;
+}
 
+double det2(const Matrix &matrix) {
+  return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+}
+double det3(const Matrix &matrix) {
+  return matrix[0][0] * matrix[1][1] * matrix[2][2] +
+      matrix[0][1] * matrix[1][2] * matrix[2][0] +
+      matrix[0][2] * matrix[1][0] * matrix[2][1] -
+      matrix[0][2] * matrix[1][1] * matrix[2][0] -
+      matrix[0][0] * matrix[1][2] * matrix[2][1] -
+      matrix[0][1] * matrix[1][0] * matrix[2][2];
+}
+
+// 计算输入matrix的行列式
+double determinant(const Matrix &matrix) {
+  if (matrix.empty()) return 1;
+  check_square(matrix);
+  size_t n = matrix.size();
+  if (n == 1) return matrix[0][0];
+  if (n == 2) return det2(matrix);
+  if (n == 3) return det3(matrix);
+  double res = 0;
+  for (int i = 0; i < n; ++i) {
+    int sign = (i & 1) ? -1 : 1;
+    Matrix minor = algebra::minor(matrix, 0, i);
+    res += matrix[0][i] * sign * determinant(minor);
+  }
+  return res;
+}
+// 求伴随矩阵
+Matrix adjoint(const Matrix &matrix) {
+  int n1 = matrix.size();
+  Matrix mat = zeros(n1, n1);
+  for (int i = 0; i < n1; ++i) {
+    for (int j = 0; j < n1; ++j) {
+      int sign = ((i + j) & 1) ? -1 : 1;
+      Matrix minor = algebra::minor(matrix, i, j);
+      mat[i][j] = sign * determinant(minor);
+    }
+  }
+  return transpose(mat);
+}
+// 生成matrix的反函数
+Matrix inverse(const Matrix &matrix) {
+  if (matrix.empty()) return matrix;
+  check_square(matrix);
+  double det = determinant(matrix);
+  if (det == 0) {
+    throw std::logic_error("singular matrices have no inverse");
+  }
+  double determinant = 1.0 / algebra::determinant(matrix);
+  return multiply(adjoint(matrix), determinant);
+}
+// 将多个矩阵以指定轴拼接
+Matrix concatenate(const Matrix &matrix1, const Matrix &matrix2, int axis) {
+  check_empty({matrix1, matrix2});
+  int n1 = matrix1.size();
+  int m1 = matrix1[0].size();
+  int n2 = matrix2.size();
+  int m2 = matrix2[0].size();
+  if (axis == 0 && m1 != m2) throw std::logic_error("column number doesn't match while axis = 0");
+  if (axis == 1 && n1 != n2) throw std::logic_error("row number doesn't match while axis = 1");
+  if (axis == 0) {
+    Matrix mat = zeros(n1 + n2, m1);
+    for (int i = 0; i < mat.size(); ++i) {
+      for (int j = 0; j < mat[0].size(); ++j) {
+        mat[i][j] = (i < n1) ? matrix1[i][j] : matrix2[i - n1][j];
+      }
+    }
+    return mat;
+  } else {
+    Matrix mat = zeros(n1, m1 + m2);
+    for (int i = 0; i < mat.size(); ++i) {
+      for (int j = 0; j < mat[0].size(); ++j) {
+        mat[i][j] = (j < m1) ? matrix1[i][j] : matrix2[i][j - m1];
+      }
+    }
+    return mat;
+  }
+}
 }
 
